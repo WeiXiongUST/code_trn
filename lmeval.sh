da217e73be7825474049c1a965d5a34d4824c94a
Xingyuan Pan <xp12@scaleml-gpu-02.cs.illinois.edu>
2024-10-24 11:07:12 -0500

    update

--- lmeval.sh
+++ lmeval.sh
@@ -12,16 +12,19 @@ accelerate launch -m lm_eval --model hf \
     --model_args pretrained=$1,dtype=bfloat16 \
     --tasks mmlu \
     --num_fewshot 5 \
-    --batch_size 4
+    --batch_size 4 \
+    --verbosity WARNING
 
 accelerate launch -m lm_eval --model hf \
     --model_args pretrained=$1,dtype=bfloat16 \
     --tasks ai2_arc \
     --num_fewshot 25 \
-    --batch_size 4
+    --batch_size 4 \
+    --verbosity WARNING
 
 accelerate launch -m lm_eval --model hf \
     --model_args pretrained=$1,dtype=bfloat16 \
     --tasks truthfulqa_mc1,truthfulqa_mc2 \
     --num_fewshot 0 \
-    --batch_size 4
+    --batch_size 4 \
+    --verbosity WARNING
